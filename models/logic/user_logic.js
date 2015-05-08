834618bc389b622ead82a750bb97e0493292bc3e
naphatkrit <naphat.krit@gmail.com>
2015-05-08 08:46:30 -0400

    added a function to update user in user logic. not tested

--- models/logic/user_logic.js
+++ models/logic/user_logic.js
@@ -2,6 +2,8 @@ var assert = require('assert');
 var Q = require('q');
 var PromiseAdapter = require('../../lib/promise_adapter');
 var PlainObject = require('./plain_object');
+var TaskGroup = require('../task_group');
+var TaskGroupLogic = require('./task_group_logic');
 var User = require('../user');
 var UserLogic;
 (function (UserLogic) {
@@ -38,5 +40,33 @@ var UserLogic;
         });
     }
     UserLogic.removeTask = removeTask;
+    function updateUser(userPlainObject) {
+        function getTaskGroupId(taskGroupPlainObject) {
+            return PromiseAdapter.convertMongooseQuery(TaskGroup.model.findOne({
+                identifier: taskGroupPlainObject.identifier
+            })).then(function (object) {
+                if (object === null) {
+                    throw new Error('not found');
+                }
+                return object.id;
+            }).fail(function (err) {
+                return TaskGroupLogic.createTaskGroup(taskGroupPlainObject).then(function (taskGroup) { return taskGroup.id; });
+            });
+        }
+        return Q.fcall(function () {
+            assert(userPlainObject !== null && userPlainObject !== undefined);
+        }).then(function () {
+            return PromiseAdapter.convertMongooseQuery(User.model.findById(userPlainObject.id));
+        }).then(function (user) {
+            assert(user.username === userPlainObject.username);
+            return Q.all(userPlainObject.taskGroups.map(getTaskGroupId)).then(function (taskGroupIds) {
+                user.taskGroups = taskGroupIds;
+                return PromiseAdapter.convertMongooseDocumentSave(user);
+            });
+        }).then(function (user) {
+            return PlainObject.convertUserInstance(user);
+        });
+    }
+    UserLogic.updateUser = updateUser;
 })(UserLogic || (UserLogic = {}));
 module.exports = UserLogic;
--- models/logic/user_logic.ts
+++ models/logic/user_logic.ts
@@ -3,6 +3,8 @@ import Q = require('q');
 
 import PromiseAdapter = require('../../lib/promise_adapter');
 import PlainObject = require('./plain_object');
+import TaskGroup = require('../task_group');
+import TaskGroupLogic = require('./task_group_logic');
 import User = require('../user');
 
 module UserLogic
@@ -45,6 +47,38 @@ module UserLogic
             return PromiseAdapter.convertMongooseDocumentSave(user);
         }).then((user) =>
         {
+            return PlainObject.convertUserInstance(user);
+        })
+    }
+    export function updateUser(userPlainObject: PlainObject.Interfaces.UserPlainObject): Q.Promise<PlainObject.Interfaces.UserPlainObject>
+    {
+        function getTaskGroupId(taskGroupPlainObject: PlainObject.Interfaces.TaskGroupPlainObject): Q.Promise<string> {
+            return PromiseAdapter.convertMongooseQuery(TaskGroup.model.findOne({
+                identifier: taskGroupPlainObject.identifier
+            })).then((object)=>{
+                if (object === null) {
+                    throw new Error('not found');
+                }
+                return object.id;
+            }).fail((err)=>{
+                return TaskGroupLogic.createTaskGroup(taskGroupPlainObject).then((taskGroup)=>{ return taskGroup.id })
+            })
+        }
+        return Q.fcall(() =>
+        {
+            assert(userPlainObject !== null && userPlainObject !== undefined);
+        }).then(() =>
+        {
+            return PromiseAdapter.convertMongooseQuery(User.model.findById(userPlainObject.id))
+        }).then((user: User.Instance) =>
+        {
+            assert(user.username === userPlainObject.username);
+            return Q.all(userPlainObject.taskGroups.map(getTaskGroupId)).then((taskGroupIds)=>{
+                // TODO what to do about tasks that belong to unenrolled task groups?
+                user.taskGroups = taskGroupIds;
+                return PromiseAdapter.convertMongooseDocumentSave(user);
+            })
+        }).then((user)=>{
             return PlainObject.convertUserInstance(user);
         })
     }
